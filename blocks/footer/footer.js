0def1077b0cbb35fab4c037f84d7a1a94730c623
Shashank Singh <shashanksing@adobe.com>
2025-02-05 14:50:25 +0530

    Updating footer block

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,7 +14,33 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  footer.classList.add('footer-content');
+  
+  // Create content for the footer
+  footer.innerHTML = `
+    <div class="footer-top">
+      <h1>WKND</h1>
+      <nav>
+        <ul>
+          <li><a href="#">MAGAZINE</a></li>
+          <li><a href="#">ADVENTURES</a></li>
+          <li><a href="#">FAQS</a></li>
+          <li><a href="#">ABOUT US</a></li>
+        </ul>
+      </nav>
+      <div class="footer-social">
+        <span>FOLLOW US</span>
+        <a href="#"><img src="/icons/facebook.svg" alt="Facebook"></a>
+        <a href="#"><img src="/icons/twitter.svg" alt="Twitter"></a>
+        <a href="#"><img src="/icons/instagram.svg" alt="Instagram"></a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>© 2019, WKND Site.</p>
+      <p>WKND is a fictitious adventure and travel website created by Adobe to demonstrate how anyone can use Adobe Experience Manager to build a beautiful, feature-rich website over a single weekend. This site is built entirely with Adobe Experience Manager <a href="#">Core Components</a> and <a href="#">Archetype</a> that are available as open source code to the public. The entire <a href="#">site source code</a> is available as open source as well and is accompanied with a <a href="#">detailed tutorial</a> on how to recreate the site.</p>
+      <p>Many of the beautiful images in the WKND site are available for purchase via <a href="#">Adobe Stock</a>.</p>
+    </div>
+  `;
 
   block.append(footer);
 }
