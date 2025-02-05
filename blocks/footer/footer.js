8f82faaf362fb717b66c9f637431ae4811b431e5
Shashank Singh <shashanksing@adobe.com>
2025-02-05 14:56:03 +0530

    fixing lint issues

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,13 +1,8 @@
-import { getMetadata } from '../../scripts/aem.js';
-
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
 
   // decorate footer DOM
   block.textContent = '';
